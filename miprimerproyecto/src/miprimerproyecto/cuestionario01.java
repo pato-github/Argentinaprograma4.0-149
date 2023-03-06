d46f2b13284f7a3da76c90e7c4883c6d5556361e
patojava <patricia.cordoba9@gmail.com>
2023-03-06 20:32:55 -0300

    vamos a subir nuestro primer commit

---
+++ miprimerproyecto/src/miprimerproyecto/cuestionario01.java
@@ -0,0 +1,16 @@
+package miprimerproyecto;
+
+public class cuestionario01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int unnum = 10;
+	while (unnum >0) {
+		System.out.println(unnum);
+		unnum = unnum -1;
+	}
+
+	}
+
+}
---
+++ miprimerproyecto/src/miprimerproyecto/miprimerclase.java
@@ -0,0 +1,22 @@
+package miprimerproyecto;
+
+public class miprimerclase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int numero = 1;
+	
+int suma = 10 + 20;
+int resta = 15-12;
+int mult = 10*3 ; 
+int div = 8/3;
+int modulo = 8%3;
+
+
+System.out.println(resta);
+
+
+   }
+}
+
